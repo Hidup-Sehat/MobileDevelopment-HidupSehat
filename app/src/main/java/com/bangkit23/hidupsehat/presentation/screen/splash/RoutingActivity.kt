8723eeeb4772e00ad25f29623e409bcb9fb36bc9
Rijal Muhyidin <71188953+rijal404@users.noreply.github.com>
2023-05-17 15:39:52 +0700

    Add RoutingActivity

---
+++ app/src/main/java/com/bangkit23/hidupsehat/presentation/screen/splash/RoutingActivity.kt
@@ -0,0 +1,12 @@
+package com.bangkit23.hidupsehat.presentation.screen.splash
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.bangkit23.hidupsehat.R
+
+class RoutingActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
