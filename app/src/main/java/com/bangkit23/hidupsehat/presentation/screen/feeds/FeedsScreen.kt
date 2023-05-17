b8a826c8f53810e44d05bf0b7a3b552e77de550e
Rijal Muhyidin <71188953+rijal404@users.noreply.github.com>
2023-05-17 15:43:47 +0700

    Add FeedsScreen

---
+++ app/src/main/java/com/bangkit23/hidupsehat/presentation/screen/feeds/FeedsScreen.kt
@@ -0,0 +1,9 @@
+package com.bangkit23.hidupsehat.presentation.screen.feeds
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FeedScreen(modifier: Modifier = Modifier) {
+
+}
