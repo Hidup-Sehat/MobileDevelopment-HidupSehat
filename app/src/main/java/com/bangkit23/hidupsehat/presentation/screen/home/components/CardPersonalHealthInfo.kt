e0239295de0231e9fe49d3637baf921b4249b48c
Rijal Muhyidin <71188953+rijal404@users.noreply.github.com>
2023-05-17 16:17:27 +0700

    Refactor with ButtonWithIcon

--- app/src/main/java/com/bangkit23/hidupsehat/presentation/screen/home/components/CardPersonalHealthInfo.kt
+++ app/src/main/java/com/bangkit23/hidupsehat/presentation/screen/home/components/CardPersonalHealthInfo.kt
@@ -7,23 +7,20 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.runtime.getValue
 import androidx.compose.material3.ElevatedCard
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.bangkit23.hidupsehat.R
+import com.bangkit23.hidupsehat.presentation.components.ButtonWithIcon
 import com.bangkit23.hidupsehat.presentation.ui.theme.HidupSehatTheme
 
 @Composable
@@ -73,33 +70,21 @@ fun ButtonInputFoods(
         horizontalArrangement = Arrangement.SpaceBetween,
         modifier = modifier.fillMaxWidth()
     ) {
-        Button(
+        ButtonWithIcon(
+            text = "Scan Foods",
+            icon = ImageVector.vectorResource(R.drawable.ic_camera),
             onClick = onScanClicked,
-            contentPadding = ButtonDefaults.ButtonWithIconContentPadding,
+            contentDescription = "Scan Foods",
             modifier = Modifier.weight(1f)
-        ) {
-            Icon(
-                painter = painterResource(R.drawable.ic_camera),
-                contentDescription = "Localized description",
-                modifier = Modifier.size(ButtonDefaults.IconSize)
-            )
-            Spacer(Modifier.size(ButtonDefaults.IconSpacing))
-            Text("Scan Foods")
-        }
+        )
         Spacer(Modifier.width(16.dp))
-        Button(
+        ButtonWithIcon(
+            text = "Manual Input",
+            icon = ImageVector.vectorResource(R.drawable.ic_create),
             onClick = onWriteManualClicked,
-            contentPadding = ButtonDefaults.ButtonWithIconContentPadding,
+            contentDescription = "Manual Input",
             modifier = Modifier.weight(1f)
-        ) {
-            Icon(
-                painter = painterResource(R.drawable.ic_create),
-                contentDescription = "Localized description",
-                modifier = Modifier.size(ButtonDefaults.IconSize)
-            )
-            Spacer(Modifier.size(ButtonDefaults.IconSpacing))
-            Text("Input Manual")
-        }
+        )
     }
 }
 
