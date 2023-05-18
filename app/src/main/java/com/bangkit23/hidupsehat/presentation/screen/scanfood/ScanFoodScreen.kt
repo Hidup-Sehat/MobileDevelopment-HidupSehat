b5a8b8646760a5fdd47f02a11ffa3e14f6d9bb9e
Rijal Muhyidin <71188953+rijal404@users.noreply.github.com>
2023-05-18 17:19:56 +0700

    Add ScanFoodScreen

---
+++ app/src/main/java/com/bangkit23/hidupsehat/presentation/screen/scanfood/ScanFoodScreen.kt
@@ -0,0 +1,11 @@
+package com.bangkit23.hidupsehat.presentation.screen.scanfood
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ScanFoodScreen(
+    modifier: Modifier = Modifier
+) {
+
+}
