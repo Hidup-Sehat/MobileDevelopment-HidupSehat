a433a5d1c5e8c51769eca58f3c2d8295014e9cf5
hafidmust <hafidalimustaqim13@gmail.com>
2023-06-12 11:26:38 +0700

    Food History

--- app/src/main/java/com/bangkit23/hidupsehat/presentation/screen/food_history/FoodHistoryScreen.kt
+++ app/src/main/java/com/bangkit23/hidupsehat/presentation/screen/food_history/FoodHistoryScreen.kt
@@ -6,42 +6,104 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.ChevronLeft
+import androidx.compose.material.icons.filled.ChevronRight
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.bangkit23.hidupsehat.R
+import com.bangkit23.hidupsehat.domain.model.food.Food
+import com.bangkit23.hidupsehat.presentation.screen.food_history.component.ItemHistory
 import com.bangkit23.hidupsehat.presentation.screen.food_history.component.ProgressHistory
 import com.bangkit23.hidupsehat.presentation.screen.food_history.component.WidgetHistoryItem
+import com.bangkit23.hidupsehat.presentation.screen.food_information_detail.component.CardFoodContent
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FoodHistoryScreen() {
-
+    val list = listOf<Food>(
+        Food(
+            id = 2887,
+            name = "Nasi Putih",
+            portionSize = "100 gram (g)",
+            energyKj = 540.0,
+            energyKKal = 129.0,
+            sugar = 0.05,
+            potassium = 35.0,
+            carbohydrate = 27.9,
+            cholesterol = 0.0,
+            fat = 0.28,
+            saturatedFat = 0.076,
+            transFat = 0.0,
+            polyunsaturatedFat = 0.075,
+            monounsaturatedFat = 0.087,
+            protein = 2.66,
+            fiber = 0.4,
+            sodium = 365.0
+        )
+    )
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Riwayat Makanan") },
+                navigationIcon = {
+                    IconButton(onClick = {}) {
+                        Icon(imageVector = Icons.Default.ArrowBack, contentDescription = null)
+                    }
+                }
+            )
+        },
+        content = {
+            FoodHistoryContent(
+                modifier = Modifier.padding(it),
+                listFoods = list
+            )
+        }
+    )
 
 }
 
 @Composable
-fun FoodHistoryContent() {
-    Column(modifier = Modifier.fillMaxSize()) {
-        Row(modifier = Modifier.fillMaxWidth(),
-        verticalAlignment = Alignment.CenterVertically,
+fun FoodHistoryContent(
+    modifier: Modifier = Modifier,
+    listFoods: List<Food>
+) {
+    Column(modifier = modifier.fillMaxSize()) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 30.dp),
+            verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceBetween
-            ) {
+        ) {
             IconButton(onClick = {}) {
-                Icon(Icons.Default.ArrowBack, contentDescription = null)
+                Icon(Icons.Default.ChevronLeft, contentDescription = null)
             }
-            Text(text = "Hari ini")
+            Text(
+                text = "Hari ini", style = MaterialTheme.typography.titleMedium.copy(
+                    fontWeight = FontWeight.Bold
+                )
+            )
             IconButton(onClick = {}) {
-                Icon(Icons.Default.ArrowForward, contentDescription = null)
+                Icon(Icons.Default.ChevronRight, contentDescription = null)
             }
         }
 
@@ -57,13 +119,38 @@ fun FoodHistoryContent() {
             )
             //circular progress bar
             ProgressHistory(
-                modifier = Modifier.size(128.dp),
-                caloriesIntakeActual = 1024.0)
+                modifier = Modifier.size(150.dp),
+                caloriesIntakeActual = 500.0,
+                caloriesIntakeExpected = 700.0
+            )
             WidgetHistoryItem(
                 data = "3000",
                 label = "Asupan max.",
                 icon = painterResource(id = R.drawable.ic_asupan_max)
             )
+        }
+
+        CardFoodContent(
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .padding(top = 20.dp)
+        )
+
+        Text(
+            modifier = Modifier.padding(horizontal = 16.dp).padding(top = 20.dp),
+            text = "Catatan Makanan", style = MaterialTheme.typography.bodyMedium.copy(
+                fontSize = 16.sp, fontWeight = FontWeight.Medium
+            )
+        )
+        Column() {
+            listFoods.forEach {
+                ItemHistory(
+                    foodName = it.name.toString(),
+                    count = it.count,
+                    portionSize = "${it.portionSize}",
+                    energyKKal = it.energyKKal ?: 0.0
+                )
+            }
         }
 
     }
@@ -72,5 +159,5 @@ fun FoodHistoryContent() {
 @Preview
 @Composable
 fun FoodHistoryContentPrev() {
-    FoodHistoryContent()
+    FoodHistoryScreen()
 }
--- app/src/main/java/com/bangkit23/hidupsehat/presentation/screen/food_history/component/ProgressHistory.kt
+++ app/src/main/java/com/bangkit23/hidupsehat/presentation/screen/food_history/component/ProgressHistory.kt
@@ -7,18 +7,21 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import com.bangkit23.hidupsehat.presentation.screen.home.components.CircularProgressBar
 
 @Composable
 fun ProgressHistory(
     caloriesIntakeActual: Double,
+    caloriesIntakeExpected : Double,
     modifier: Modifier = Modifier
 ) {
-    val progress by remember { mutableStateOf((caloriesIntakeActual) * 100.0) }
+    val progress by remember { mutableStateOf((caloriesIntakeActual / caloriesIntakeExpected) * 100.0) }
     val percentage by remember { mutableStateOf(progress / 100.0) }
     ConstraintLayout(modifier = modifier) {
         val (progressBar, caloriesIntake, calorieText) = createRefs()
@@ -32,17 +35,18 @@ fun ProgressHistory(
         )
         Text(
             text = "${caloriesIntakeActual.toInt()}",
-            style = MaterialTheme.typography.titleLarge,
+            style = MaterialTheme.typography.titleLarge.copy(
+                fontWeight = FontWeight.Bold, fontSize = 32.sp
+            ),
             overflow = TextOverflow.Visible,
             modifier = Modifier.constrainAs(caloriesIntake) {
-                top.linkTo(progressBar.top)
-                bottom.linkTo(progressBar.bottom)
+                top.linkTo(progressBar.top, margin = 40.dp)
                 start.linkTo(progressBar.start)
                 end.linkTo(progressBar.end)
             }
         )
         Text(
-            text = "kkal",
+            text = "Asupan Kalori",
             style = MaterialTheme.typography.bodyMedium,
             modifier = Modifier.constrainAs(calorieText) {
                 top.linkTo(caloriesIntake.bottom)
@@ -56,5 +60,5 @@ fun ProgressHistory(
 @Preview
 @Composable
 fun ProgressHistoryPrev() {
-    ProgressHistory(caloriesIntakeActual = 1024.0)
+    ProgressHistory(caloriesIntakeActual = 1024.0, caloriesIntakeExpected = 7000.0)
 }
